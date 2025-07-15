e29c6fa3c9146cbc0793ad581b9aea0dfd1a7172
Anar Rustamov <44277604+anar1984@users.noreply.github.com>
2025-07-15 19:06:16 +0000

    my first project commited

---
+++ hello-world/src/App.js
@@ -0,0 +1,12 @@
+ 
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <label style={{fontSize:"30px",color:"red"}}>It's my first project.</label>
+   </div>  
+  )
+}
+
+export default App;
